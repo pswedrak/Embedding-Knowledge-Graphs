156878de006e2210082d8b465884580b7c5f2e0f
pswedrak <piotrekswedrak@gmail.com>
2020-12-18 00:17:26 +0100

    Visualize heatmap of similarity

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from sentiment_analysis.simon import compute_simon_vector
 from text_processing.text_utils import process_text
+import time
 
 
 def main():
+    start = time.time()
     input_tokens = process_text("texts/review_short.txt")
-    simon_vector = compute_simon_vector(input_tokens, 50)
+    simon_vector = compute_simon_vector(input_tokens, 10)
+    end = time.time()
+    #print(end - start)
+    print(simon_vector)
 
 
 if __name__ == "__main__":
--- sentiment_analysis/simon.py
+++ sentiment_analysis/simon.py
@@ -2,11 +2,15 @@ import random
 import numpy as np
 from nltk.corpus import wordnet as wn
 from nltk.corpus import opinion_lexicon
+from sematch.semantic.similarity import WordNetSimilarity
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def compute_simon_vector(input_tokens, k=25):
     lexicon_words = extract_lexicon_words(k)
-    similarity_matrix = np.zeros((len(input_tokens), len(lexicon_words)))
+    wns = WordNetSimilarity()
+    print(lexicon_words)
 
     input_tokens_in_wordnet = []
     for input_token in input_tokens:
@@ -14,15 +18,21 @@ def compute_simon_vector(input_tokens, k=25):
             input_tokens_in_wordnet.append(input_token)
     input_tokens = input_tokens_in_wordnet
 
+    similarity_matrix = np.zeros((len(input_tokens), len(lexicon_words)))
+    print(wns.word_similarity('film', 'great', 'wpath'))
+
     for i in range(len(input_tokens)):
         for j in range(len(lexicon_words)):
-            similarity_matrix[i, j] = 1
+            similarity_matrix[i, j] = wns.word_similarity(input_tokens[i], lexicon_words[j], 'wpath')
+
+    sns.heatmap(similarity_matrix, xticklabels=lexicon_words, yticklabels=input_tokens)
+    plt.show()
 
     return np.max(similarity_matrix, axis=0)
 
 
 def extract_lexicon_words(size):
     positive_words = random.sample(list(opinion_lexicon.positive()), size//2)
-    negative_words = random.sample(list(opinion_lexicon.positive()), size - size // 2)
+    negative_words = random.sample(list(opinion_lexicon.negative()), size - size // 2)
 
     return positive_words + negative_words
